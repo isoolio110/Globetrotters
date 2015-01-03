22030db4d5ac25ef86f2852010d5fca087ce31d8
isoolio110 <kim.isul@gmail.com>
2015-01-03 17:58:32 -0500

    Adds data on the most popular destinations to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+MostPopularDestination.create(destination: 'Grand Bazaar, Istanbul', num_of_visitors:91250000)
+MostPopularDestination.create(destination: 'The Zócalo, Mexico City', num_of_visitors:85000000)
+MostPopularDestination.create(destination: 'Times Square, New York City', num_of_visitors:50000000)
+MostPopularDestination.create(destination: 'Central Park, New York City', num_of_visitors:40000000)
+MostPopularDestination.create(destination: 'Union Station, Washington, DC', num_of_visitors:40000000)
+MostPopularDestination.create(destination: 'Las Vegas Strip', num_of_visitors:30500000)
+MostPopularDestination.create(destination: 'Meiji Jingu Shrine, Tokyo', num_of_visitors:30000000)
+MostPopularDestination.create(destination: 'Sensoji Temple, Tokyo', num_of_visitors:30000000)
+MostPopularDestination.create(destination: 'Niagara Falls, Ontario', num_of_visitors:22000000)
+MostPopularDestination.create(destination: 'Grand Central Terminal, New York City', num_of_visitors:21600000)
