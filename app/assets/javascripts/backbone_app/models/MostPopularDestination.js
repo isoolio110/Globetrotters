7b04b7aebca5fb923bdb786b827db326e3575503
isoolio110 <kim.isul@gmail.com>
2015-01-03 18:38:54 -0500

    Adds MostPopularDestination backbone model

---
+++ app/assets/javascripts/backbone_app/models/MostPopularDestination.js
@@ -0,0 +1,10 @@
+console.log("loaded MostPopularDestination.js - top");
+
+var MostPopularDestination = Backbone.Model.extend({
+  defaults: {
+    destination: '',
+    num_of_visitors: ''
+  }
+});
+
+console.log("loaded MostPopularDestination.js - bottom");
