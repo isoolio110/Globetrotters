6a8e2799e3f765cdfeacf2438d0593fac9e42aa8
isoolio110 <kim.isul@gmail.com>
2015-01-03 18:26:19 -0500

    Adds custom filter function to User backbone collection

--- app/assets/javascripts/backbone_app/collections/UserList.js
+++ app/assets/javascripts/backbone_app/collections/UserList.js
@@ -4,6 +4,11 @@ var UsersList = Backbone.Collection.extend({
   url: '/users',
   model: User
 
+  customFilter: function(filter) {
+  var results = this.where(filter);
+  return new UsersList(results);
+  }
+
 });
 
 console.log("loaded UsersList.js - bottom");
