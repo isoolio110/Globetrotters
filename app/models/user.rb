6a5904dc9c23396337b677f6ed081c1ebafc4561
isoolio110 <kim.isul@gmail.com>
2015-01-03 14:02:21 -0500

    Adds Paperclip-specific code to User model to indicate that users have attachments and to validate the attachments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  # Paperclip
+  has_attached_file :avatar, :styles => { :medium => "200x200#", :thumb => "100x100#" }, :default_url => "avatars/missing.jpg", :convert_options => {:thumb => '-set colorspace sRGB -strip', :medium => '-set colorspace sRGB -strip' }
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
