93b2f0b47d5be7d02ed84f663fde93e816f90403
isoolio110 <kim.isul@gmail.com>
2015-01-03 16:09:11 -0500

    Makes correction to inherit from ActionController::Base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ApplicationController
+class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   protect_from_forgery with: :exception
 
