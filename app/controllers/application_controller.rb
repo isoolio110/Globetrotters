5f065dddd0b3c4c51852e8a67f064c2c3a3dc45b
isoolio110 <kim.isul@gmail.com>
2015-01-03 16:07:16 -0500

    Adds ApplicationController with an authorize user method and a current_user method to find the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authorize
+    redirect_to root_path unless current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
 end
