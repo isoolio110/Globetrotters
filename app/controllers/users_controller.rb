bd76621067e00baade0e60b86991bdb08a8c8be8
isoolio110 <kim.isul@gmail.com>
2015-01-03 14:46:57 -0500

    Creates an api for users and allows for new users to be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.all
+    user_info = []
+    @users.each do |user|
+      if user != current_user
+      user_info << {id: user.id, current_user: 0, username: user.username, image_url: user.avatar.url(:medium)}
+      else
+      user_info << {id: user.id, current_user: 1, username: user.username, image_url: user.avatar.url(:medium)}
+      end
+    end
+    render json: user_info.to_json
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    user = User.create(user_params)
+    if user.valid?
+      redirect_to "/login"
+    else
+      @errors = user.errors.full_messages
+      @user = User.new
+      render "new"
+    end
   end
 
   def edit
