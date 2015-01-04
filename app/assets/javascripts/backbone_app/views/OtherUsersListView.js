d51b31f808522a0c8b2805caef8eb230e1b5aa18
isoolio110 <kim.isul@gmail.com>
2015-01-03 23:39:38 -0500

    Adds OtherUsersList backbone view to render the collection of other users who have written stories about pllaces that the user is interested in

---
+++ app/assets/javascripts/backbone_app/views/OtherUsersListView.js
@@ -0,0 +1,21 @@
+console.log('loaded OtherUsersListView.js - top')
+
+var OtherUsersListView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'other-users-list',
+  template: _.template($('#other-users-list-template').html()),
+
+  initialize: function(){
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  render: function(){
+    var renderedHTML = this.template({
+      otherUsers: this.collection});
+    this.$el.html(renderedHTML);
+    return this;
+  }
+  
+});
+
+console.log('loaded OtherUsersListView.js - bottom')
