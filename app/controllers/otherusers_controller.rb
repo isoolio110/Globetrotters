77d49ad41a6dcfa18903b03c677eab3b30a1ba3c
isoolio110 <kim.isul@gmail.com>
2015-01-03 20:40:09 -0500

    Adds OtherUsersController which serves up data about other users that have written stories about the places that the user wants to visit

---
+++ app/controllers/otherusers_controller.rb
@@ -0,0 +1,31 @@
+class OtherusersController < ApplicationController
+
+  def index
+    otherusers = []
+    UserLocation.find_each do |location|
+      Story.where(location: location.location).find_each do |story|
+        otherusers << {user_id: location.user_id, location: location.location, other_user_id: story.user_id}
+      end
+    end
+
+    otherusersinfo = []
+    otherusers.each do |otheruser|
+      User.where(id: otheruser[:other_user_id]).find_each do |user|
+          otherusersinfo << {user_id: otheruser[:user_id], location: otheruser[:location], other_user_id: otheruser[:other_user_id], username: user.username, image_url: user.avatar.url(:medium)}
+      end
+    end
+
+    otherusersinfofiltered = []
+    otherusersinfo.each do |otherusersinfo|
+      if otherusersinfo[:user_id] != otherusersinfo[:other_user_id]
+        otherusersinfofiltered << {user_id: otherusersinfo[:user_id], other_user_id: otherusersinfo[:other_user_id], username: otherusersinfo[:username], image_url: otherusersinfo[:image_url] }
+      end
+    end
+
+    otherusersinfofiltered = otherusersinfofiltered.uniq{|x| x}
+
+    render json: otherusersinfofiltered.to_json
+
+  end
+
+end
