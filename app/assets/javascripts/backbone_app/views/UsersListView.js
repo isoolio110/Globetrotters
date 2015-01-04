27bee96e63b987b268ec007ad2908c04da37ccb2
isoolio110 <kim.isul@gmail.com>
2015-01-03 23:54:11 -0500

    Adds UsersListView that passes the users collection to a template to render a profile picture on the profile page

---
+++ app/assets/javascripts/backbone_app/views/UsersListView.js
@@ -0,0 +1,21 @@
+console.log('loaded UsersListView.js - top')
+
+var UsersListView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'profile-pic',
+  template: _.template($('#users-list-template').html()),
+
+  initialize: function(){
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  render: function(){
+    var renderedHTML = this.template({
+      users: this.collection});
+    this.$el.html(renderedHTML);
+    return this;
+  }
+  
+});
+
+console.log('loaded UsersListView.js - bottom')
