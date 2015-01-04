488e8fb4cd060a2e1bed3c61a1effc9ab80ee9f0
isoolio110 <kim.isul@gmail.com>
2015-01-03 23:22:20 -0500

    Adds StoryList backbone view to render the collection of stories in the appropriate template. The view also creates a new story when a user submits a story.

---
+++ app/assets/javascripts/backbone_app/views/StoryListView.js
@@ -0,0 +1,39 @@
+console.log('loaded StoryListView.js - top')
+
+var StoryListView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'stories-list',
+  template1: _.template($('#story-list-1-template').html()),
+  template2: _.template($('#story-list-2-template').html()),
+
+  initialize: function(options){
+    this.template = this['template' + options.template_number];
+    this.collection = options.collection;
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  render: function(){
+    $("#landing-pg-main-div").hide();
+    var renderedHTML = this.template({
+      stories: this.collection});
+    this.$el.html(renderedHTML);
+    return this;
+  },
+
+  events: {
+    'submit form': 'onSubmit'
+  },
+
+  onSubmit: function(e){
+    e.preventDefault();
+      var title = this.$('[name="Title"]').val();
+    var location = this.$('[name="Location"]').val();
+    var description = this.$('[name="Description"]').val();
+    var packlist = this.$('[name="PackList"]').val();
+    this.collection.create({title: title, location: location, description: description, packlist: packlist
+    });
+  }
+
+});
+
+console.log('loaded StoryListView.js - bottom')
