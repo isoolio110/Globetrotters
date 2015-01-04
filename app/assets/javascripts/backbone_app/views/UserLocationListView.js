5826cfb9a11c71980b7f32913489b521c77db854
isoolio110 <kim.isul@gmail.com>
2015-01-03 19:24:52 -0500

    Adds UserLocationList backbone view to render the collection in the template. The view also has a function to create an userlocation when a location is added to a form on the page

---
+++ app/assets/javascripts/backbone_app/views/UserLocationListView.js
@@ -0,0 +1,50 @@
+console.log('loaded UserLocationListView.js - top')
+
+var UserLocationListView = Backbone.View.extend({
+  // creates a div
+  tagName: 'div',
+  // the div will have a classname of below
+  className: 'userlocations-list',
+  // the underscore template id is from the script tag on the html
+  template: _.template($('#user-location-list-template').html()),
+
+  // set up a listener
+  // the view is told which collection to listen to in the main.js file
+  initialize: function(){
+    this.listenTo(this.collection, 'sync', this.render);
+  },
+
+  // render means to populate the template with information from this collection
+  render: function(){
+    $("#landing-pg-main-div").hide();        
+    $('#map-canvas').show();
+    $('#bar-chart').show();
+    $('#user-locations-container').show();    
+    // the html data to be rendered is the data from the collection into the underscore template
+    var renderedHTML = this.template({
+      userlocations: this.collection});
+    // put the data to be rendered into the list views html
+    this.$el.html(renderedHTML);
+    // show the html
+    return this;
+  },
+
+  // backbone way of writing events
+  events: {
+    'submit form': 'onSubmit'
+  },
+
+  // the onSubmit function states that when the event occurs: 
+  onSubmit: function(e){
+    // prevent default action
+    e.preventDefault();
+    // get the value from the form where the name of the input tag is goal and assign it to the variable goal
+    var location = this.$('[name="location"]').val();
+    // var planned_date = this.$('[name="planned_date"]').val();
+    this.collection.create({location: location
+      // ,planned_date: planned_date
+    });
+  }
+});
+
+console.log('loaded UserLocationListView.js - bottom')
