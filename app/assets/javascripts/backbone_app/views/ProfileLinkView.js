2f0806bd44351ad5ac2c292ff71f86ea6fb07c24
isoolio110 <kim.isul@gmail.com>
2015-01-03 23:09:44 -0500

    Adds ProfileLink backbone view to pass the current user data to the profile link template

---
+++ app/assets/javascripts/backbone_app/views/ProfileLinkView.js
@@ -0,0 +1,24 @@
+console.log('loaded ProfileLinkView.js - top')
+
+var ProfileLinkView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'profile-link',
+  template: _.template($('#profile-link-template').html()),
+
+  initialize: function(options){
+      this.currentUserID = options.currentUserID 
+      this.currentUserUsername = options.currentUserUsername 
+      this.currentUserImageURL = options.currentUserImageURL
+  },
+
+  render: function(){
+    var renderedHTML = this.template({
+      currentUserID: this.currentUserID, currentUserUsername: this.currentUserUsername, currentUserImageURL: this.currentUserImageURL
+    });
+    this.$el.html(renderedHTML);
+    return this;
+  }
+
+});
+
+console.log('loaded ProfileLinkView.js - bottom')
