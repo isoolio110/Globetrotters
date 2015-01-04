c27118398c087e79867b889d4dab560074a0516c
isoolio110 <kim.isul@gmail.com>
2015-01-03 19:53:33 -0500

    Adds functions to backbone router to instantiate a new MostPopularDestination view with the MostPopularDestination collection and to render that view

--- app/assets/javascripts/backbone_app/routers/AppRouter.js
+++ app/assets/javascripts/backbone_app/routers/AppRouter.js
@@ -24,7 +24,24 @@ var AppRouter = Backbone.Router.extend({
     var userSpecificLocations = this.usersLocationCollection.customFilter({"user_id": parseInt(id)});
     var view = new UserLocationListView({collection: userSpecificLocations}); 
     this.setLocationsListView(view);
+  },
+
+  setBarChartView: function(newView) {
+    this.view = newView;
+    $('#most-popular-destinations-chart-container').html(this.view.render().$el);
+  },
+
+  showBarChart: function() {
+    var view = new MostPopularDestinationView({collection: this.topDestinationsCollection});
+    this.setBarChartView(view);
+  },
+
+  userLocation: function(id){
+    this.userLocationsList(parseInt(id));
+    this.showBarChart();
   }
+
+
 
 });
 
