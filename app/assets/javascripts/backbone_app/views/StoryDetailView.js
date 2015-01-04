2af7500ee425083b61195f00a66c0d6d86f8b256
isoolio110 <kim.isul@gmail.com>
2015-01-04 00:03:47 -0500

    Adds StoryDetail backbone view to render a form for editting or removing a story.

---
+++ app/assets/javascripts/backbone_app/views/StoryDetailView.js
@@ -0,0 +1,57 @@
+console.log('loaded StoryDetailListView.js - top')
+
+var StoryDetailView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'story-detail',
+  template: _.template($('#story-detail-template').html()),
+
+  render: function() {
+  $("#landing-pg-main-div").hide(); 
+  $('#map-canvas').hide();
+  $('#profile-pg-profile-link-container').hide();
+  $('#profile-pg-profile-pic-container').hide();
+  $('#profile-pg-travel-agenda-container').hide();
+  $('#profile-pg-stories-container').hide();
+  $('#profile-pg-other-users-container').hide();
+  $('#story-detail-container').show();
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  },
+
+  done: function() {
+  var userId = this.model.get('user_id')
+    window.location.href = '#profiles/'+userId;
+  },
+
+  events: {
+    'click #done': 'onDone',
+    'click #remove': 'onRemove',
+    'click #save': 'onSave'
+  },
+
+  onDone: function(evt) {
+    evt.preventDefault();
+    this.done();
+    return this;
+  },
+
+  onSave: function(evt) {
+    evt.preventDefault();
+    var data = this.$('input').serializeObject();
+    this.model.save(data);
+    this.done();
+    return this;
+  },
+
+  onRemove: function(evt) {
+    evt.preventDefault();
+    if (window.confirm('Are you sure you want to delete this story?')) {
+      this.model.destroy();
+      this.done();
+    }
+    return this;
+  }
+
+});
+
+console.log('loaded StoryDetailListView.js - bottom')
