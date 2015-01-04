0d51c7e411ac59716af7c10700fa6ad8ed37504c
isoolio110 <kim.isul@gmail.com>
2015-01-03 21:05:18 -0500

    Adds custom filter function to the OtherUsers backbone collection

--- app/assets/javascripts/backbone_app/collections/OtherUsersList.js
+++ app/assets/javascripts/backbone_app/collections/OtherUsersList.js
@@ -2,8 +2,13 @@ console.log("loaded OtherUsersList.js - top");
 
 var OtherUsersList = Backbone.Collection.extend({
   url: '/otherusers',
-  model: OtherUser
-  
+  model: OtherUser,
+
+  customFilter: function(filter) {
+  var results = this.where(filter);
+  return new OtherUsersList(results);
+  }
+
 });
 
 console.log("loaded OtherUsersList.js - bottom");
